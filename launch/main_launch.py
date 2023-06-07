a9bc2909d189ce4ae92592b55d73e9f7a6b3afeb
Patrick Creighton <pcreighton429@gmail.com>
2023-06-07 07:25:49 +0000

    Add log_level launch argument

--- launch/main_launch.py
+++ launch/main_launch.py
@@ -4,13 +4,22 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     global_config = os.path.join(get_package_share_directory('local_pathfinding'), 'globals.yaml')
 
+    log_level = LaunchConfiguration('log_level')
+
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                name='log_level',
+                default_value=['info'],
+                description='Logging level',
+            ),
             Node(
                 package='local_pathfinding',
                 executable='sailbot',
@@ -18,6 +27,11 @@ def generate_launch_description():
                 output='screen',
                 emulate_tty=True,
                 parameters=[global_config],
-            )
+                arguments=[
+                    "--ros-args",
+                    "--log-level",
+                    ["sailbot_main:=", log_level],
+                ],
+            ),
         ]
     )
