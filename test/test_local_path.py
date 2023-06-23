b1829fe340add9254f2a902325b4fcb752ce6bc4
Patrick Creighton <pcreighton429@gmail.com>
2023-06-23 15:59:53 -0700

    Write initial local path tests

---
+++ test/test_local_path.py
@@ -0,0 +1,17 @@
+from custom_interfaces.msg import GPS, AISShips, GlobalPath, WindSensor
+from rclpy.impl.rcutils_logger import RcutilsLogger
+
+import local_pathfinding.local_path as local_path
+
+PATH = local_path.LocalPath(parent_logger=RcutilsLogger())
+
+
+def test_LocalPath_update_if_needed():
+    PATH.update_if_needed(
+        gps=GPS(),
+        ais_ships=AISShips(),
+        global_path=GlobalPath(),
+        filtered_wind_sensor=WindSensor(),
+    )
+    assert PATH.waypoints is not None, 'waypoints is not initialized'
+    assert len(PATH.waypoints) > 1, 'waypoints length <= 1'
