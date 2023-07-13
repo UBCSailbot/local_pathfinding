1a497d38fd80d7f87a2f87e74d30ec5c4ba1e423
Akshanjay Kompelli <Akshanjay2004@gmail.com>
2023-07-13 10:14:18 -0700

    Changed some of the Docstrings

--- local_pathfinding/local_path.py
+++ local_pathfinding/local_path.py
@@ -4,19 +4,24 @@ from custom_interfaces.msg import GPS, AISShips, GlobalPath, WindSensor
 from rclpy.impl.rcutils_logger import RcutilsLogger
 
 from local_pathfinding.ompl_path import OMPLPath
+from typing import List, Tuple
 
 
 class LocalPathState:
     """Gathers and stores the state of the Sailbot.
 
     Attributes:
-        `postion` (GPS): The latitude (float32) and longitudinal (float32) coordinates of the Sailbot.
-        `speed` (GPS): The speed (float32) of the Sailbot at that position. Units: km/hr.
-        `heading` (GPS): The direction (float32) in which the Sailbot is Sailing at. Units: Degrees.
-        `ais_ships` (AISShips): List of ships.
-        `global_path` (GlobalPath): Objects of all the global way points which the Sailbot will travel to.
-        `wind_speed` (WindSensor): The wind speed. Units: km/hr.
-        `wind_direction` (WindSensor): The wind direction (int16) towards the boat. Units: Degrees.
+        `position` (GPS) (Tuple[float, float]): The latitude and longitudinal coordinates of the
+         Sailbot.
+        `speed` (GPS) (float): The speed of the Sailbot at that position.
+        `heading` (GPS) (float): The direction in which the Sailbot is Sailing at.
+        `ais_ships` (AISShips) (List): List of ships.
+        `global_path` (GlobalPath) (List): Objects of all the global way points which the Sailbot will
+         travel to.
+        `wind_speed` (WindSensor) (float): The wind speed.
+        `wind_direction` (WindSensor) (int): The wind direction towards the boat.
+        The attributes' units and conventions can be found in the ROS msgs, which are derived from
+        custom_interfaces.
     """
 
     def __init__(
@@ -26,13 +31,13 @@ class LocalPathState:
         global_path: GlobalPath,
         filtered_wind_sensor: WindSensor,
     ):
-        """Initializes the local path state.
+        """Initializes the local path finding state from the ROS msgs.
 
         Args:
-            `gps` (GPS): Data from the GPS sensors.
-            `ais_ships` (AISShips): Data from the AIS receiver.
-            `global_path` (GlobalPath): Data from the Globalpath server.
-            `filtered_wind_sensor` (WindSensor): Data from the windsensors.
+            `gps`: The GPS msg.
+            `ais_ships`: The AISShips msg.
+            `global_path`: The GlobalPath msg.
+            `filtered_wind_sensor`: The WindSensor msg.
         """
         if gps:  # TODO: remove when mock can be run
             self.position = (gps.lat_lon.latitude, gps.lat_lon.longitude)
@@ -41,6 +46,7 @@ class LocalPathState:
 
         if ais_ships:  # TODO: remove when mock can be run
             self.ais_ships = [ship for ship in ais_ships.ships]
+
 
         if global_path:  # TODO: remove when mock can be run
             self.global_path = [
@@ -76,10 +82,10 @@ class LocalPath:
            The path is updated if a new path is found.
 
         Args:
-            `gps` (GPS): Data from the GPS sensors.
-            `ais_ships` (AISShips): Data from the AIS receiver.
-            `global_path` (GlobalPath): Data from the Globalpath server.
-            `filtered_wind_sensor` (WindSensor): Data from the windsensors.
+            `gps`: The GPS msg.
+            `ais_ships`: The AISShips msg.
+            `global_path`: The GlobalPath msg.
+            `filtered_wind_sensor`: The WindSensor msg.
         """
         state = LocalPathState(gps, ais_ships, global_path, filtered_wind_sensor)
         ompl_path = OMPLPath(parent_logger=self._logger, max_runtime=1.0, local_path_state=state)
