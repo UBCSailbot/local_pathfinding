c33439e1469052791a5388be561b62d82cab628c
Patrick Creighton <pcreighton429@gmail.com>
2023-07-29 17:11:56 -0700

    Support global launch file

--- launch/main_launch.py
+++ launch/main_launch.py
@@ -1,52 +1,74 @@
+import importlib
 import os
+from types import ModuleType
 from typing import List
 
-from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
-from launch.logging import launch_config
 from launch.substitutions import LaunchConfiguration
 
 
-def generate_launch_description():
-    global_config = os.path.join(get_package_share_directory('local_pathfinding'), 'globals.yaml')
+def get_global_main_launch_module() -> ModuleType:
+    """Execute and return the main launch file of the `global_launch` package.
 
-    log_level = LaunchConfiguration('log_level')
+    Returns:
+        ModuleType: The executed module.
+    """
+    global_main_launch = os.path.join(
+        os.getenv("ROS_WORKSPACE"), "src", "global_launch", "main_launch.py"
+    )
+    spec = importlib.util.spec_from_file_location("global_launch", global_main_launch)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+GLOBAL_LAUNCH = get_global_main_launch_module()
+
+
+def generate_launch_description() -> LaunchDescription:
+    """Generate the launch description for the `local_pathfinding` package.
+
+    Returns:
+        LaunchDescription: The launch description.
+    """
+    # get_nodes() arguments for the package launch
+    common_parameters = [GLOBAL_LAUNCH.GLOBAL_CONFIG]
+    common_ros_arguments = [*GLOBAL_LAUNCH.get_log_ros_arguments()]
+    mode = LaunchConfiguration("mode")
 
     return LaunchDescription(
         [
-            # put node log files in same directory as launch log files
-            # ref: https://github.com/ros2/launch/issues/551#issuecomment-982146452
-            SetEnvironmentVariable('ROS_LOG_DIR', launch_config.log_dir),
-            # ref: https://answers.ros.org/question/311471/selecting-log-level-in-ros2-launch-file/
-            DeclareLaunchArgument(
-                name='log_level',
-                default_value=['info'],
-                description='Logging level',
-            ),
-            *get_nodes(
-                global_parameters=[global_config],
-                global_arguments=[
-                    "--ros-args",
-                    "--log-level",
-                    ["navigate_main:=", log_level],
-                ],
-            ),
+            *GLOBAL_LAUNCH.GLOBAL_LAUNCH_ARGUMENTS,
+            *get_nodes(common_parameters, common_ros_arguments, mode),
         ]
     )
 
 
-def get_nodes(global_parameters: List = [], global_arguments: List = []) -> List[Node]:
+def get_nodes(common_parameters: List, common_ros_arguments: List, mode: str) -> List[Node]:
+    """Get the nodes to be launched depending on an indicated mode.
+
+    Args:
+        common_parameters (List): Parameters that are common to all nodes.
+        common_ros_arguments (List): ROS arguments that are common to all nodes.
+        mode (str): The system mode.
+
+    Returns:
+        List[Node]: The nodes to be launched.
+    """
+    # navigate_main parameters and ROS arguments
+    navigate_main_parameters = [*common_parameters]
+    navigate_main_ros_arguments = [*common_ros_arguments]
+
     return [
         Node(
-            package='local_pathfinding',
-            executable='navigate',
-            name='navigate_main',
-            output='screen',
+            package="local_pathfinding",
+            executable="navigate",
+            name="navigate_main",
+            output="screen",
             emulate_tty=True,
-            parameters=[*global_parameters],
-            arguments=[*global_arguments],
+            parameters=navigate_main_parameters,
+            ros_arguments=navigate_main_ros_arguments,
         ),
     ]
