032a3e4bac3da96cc9d609becce2fd76bd43f73b
SPDonaghy <118148642+SPDonaghy@users.noreply.github.com>
2023-10-03 16:16:28 -0700

    Added hull to boat attributes, added a test file

--- local_pathfinding/obstacles.py
+++ local_pathfinding/obstacles.py
@@ -1,31 +1,23 @@
 import numpy as np
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Polygon
 
 
-class Obstacle(Polygon):
+class ObstacleInterface:
 
     """
-    This class describes general obstacle objects which are anything which the sailbot must avoid.
-    Obstacles are a child class of Polygon.
+    This Interface describes general obstacle objects which are
+    anything which the sailbot must avoid.
+
+    The choice was made to create an interface rather than a class as Boat and LandMass objects
+    may be very different objects which share an "avoidable" property, but not much else.
 
     For now, it serves as a placeholder for future functionality, in case we need to collect all
     obstacles, of different types, in one place.
 
     """
 
-    def __init__(self, position):
-        """
-        Obstacle constructor
 
-        Args
-        ----------
-        position (NamedTuple) : x,y coordinates of the obstacle converted from lat/lon
-
-        """
-        self.position = position
-
-
-class Boat(Obstacle):
+class Boat(ObstacleInterface):
 
     """
     This class describes boat objects.
@@ -33,14 +25,13 @@ class Boat(Obstacle):
     Attributes
     ----------
         -id (int): MMSI number of the boat
-        -position (NamedTuple): x,y coordinates of the boat converted from lat/lon
         -speed (float): speed of the boat in knots
-        -true bearing (float): heading of the boat in degrees measured clockwise from true north
-        -width (float): width of the boat in meters
-        -length (float): length of the boat in meters
         -shell (Polygon): a shapely Polygon object representing the boat's hull
             -the shell is orientated according to the heading of the boat
-
+            -position (XY): x,y coordinates of the boat's center (converted from lat/lon)
+            -true bearing (float): heading of the boat in degrees, clockwise from true north
+            -width (float): width of the boat in meters
+            -length (float): length of the boat in meters
     Notes
     ----------
         The following information about a boat is obtained from AIS:
@@ -51,37 +42,69 @@ class Boat(Obstacle):
             - heading (degrees measured clockwise from true north)
             - other information is also available but not used here
 
+        To avoid confusion, the physical dimensions, position, and heading of the boat are
+        stored in the aggregated Polygon object, not the Boat object itself.
+        So that all we need to do to update the boat's position or true bearing is to update the
+        polygon's position and rotation.
+
     """
 
-    def __init__(self, id, position, speed, true_bearing, width, length):
+    def __init__(self, id, width, length, position, speed, true_bearing):
         """
         Args:
 
-            id (int): MMSI number of the boat
-            -position (NamedTuple): x,y coordinates of the boat converted from lat/lon
-            -speed (float): speed of the boat in knots
-            -true_bearing (float, degrees): heading of the boat measured clockwise from true north
+            -id (int): MMSI number of the boat
             -width (float): width of the boat in meters
             -length (float): length of the boat in meters
+            -position (XY): x,y coordinates of the boat converted from lat/lon
+            -speed (float): speed of the boat in knots
+            -true_bearing (float, degrees): heading of the boatclockwise from true north
         """
-        super().__init__(position)
         self.id = id
         self.speed = speed
-        self.true_bearing = true_bearing
-        self.width = width
-        self.length = length
-        self.shell = self.create_hull()
+        self.hull = self.create_hull(position, width, length, true_bearing)
 
-    def create_hull(self, position, width, length, heading):
+    def create_hull(self, position, width, length, true_bearing) -> Polygon:
         """
-        creates a Shapely Shell to represent the boat's hull, which is orientated and positioned
+        creates a Shapely Polygon to represent the boat's hull, which is orientated and positioned
         according to the boat's true bearing and position.
 
         Args:
 
-            position (NamedTuple): x,y coordinates of the boat, converted from lat/lon
+            position (XY): x,y coordinates of the boat, converted from lat/lon
             width (float): width of the boat in meters
             length (float): length of the boat in meters
-            heading (float): heading of the boat in degrees measured clockwise from true north
+            true_bearing (float): heading of the boat in degrees clockwise from true north
         """
-        # TODO implement this
+        # coordinates of the center of the boat
+        x, y = position[0], position[1]
+
+        # Points of the boat polygon before rotation and centred at the origin
+        points = np.array(
+            [
+                [-width / 2, -length / 2],
+                [-width / 2, length / 2],
+                [width / 2, length / 2],
+                [width / 2, -length / 2],
+                1,
+            ]
+        )
+
+        # Rotation matrix
+        # according to napkin math, the rotation matrix should be able to
+        # use the true_bearing angle directly
+        # but #TODO: check this
+        rot = np.array(
+            [
+                [np.cos(true_bearing), -np.sin(true_bearing)],
+                [np.sin(true_bearing), np.cos(true_bearing)],
+            ]
+        )
+
+        # rotate the points about the origin, to orientate the boat according to its heading
+        points = points @ rot
+
+        # translate the points to the boat's position
+        points = points + np.array([x, y])
+
+        return Polygon(points)
---
+++ test/test_obstacles.py
@@ -0,0 +1,5 @@
+from local_pathfinding.obstacles import Boat
+
+boat1 = Boat(1, 10, 10, (4, 5), 10, 0)
+
+print(boat1.shell)
