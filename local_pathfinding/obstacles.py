149e72ff381788ac8eacc5ad158c2851211cc9ff
SPDonaghy <118148642+SPDonaghy@users.noreply.github.com>
2023-10-03 12:19:30 -0700

    Outlines and constructors for Obstacle and Boat

--- local_pathfinding/obstacles.py
+++ local_pathfinding/obstacles.py
@@ -15,13 +15,13 @@ class Obstacle(Polygon):
 
     """
 
-    def __init__(self, position) -> None:
+    def __init__(self, position):
         """
         Obstacle constructor
 
-        Parameters
+        Args
         ----------
-        position : x,y coordinates of the obstacle (likely converted from lat/lon)
+        position (NamedTuple) : x,y coordinates of the obstacle converted from lat/lon
 
         """
         self.position = position
@@ -31,40 +31,59 @@ class Boat(Obstacle):
 
     """
     This class describes boat objects.
-    Boats may be stationary or moving. If moving, they have a speed and heading.
 
+    Attributes
+    ----------
+        -id (int): MMSI number of the boat
+        -position (NamedTuple): x,y coordinates of the boat converted from lat/lon
+        -speed (float): speed of the boat in knots
+        -true bearing (float): heading of the boat in degrees measured clockwise from true north
+        -width (float): width of the boat in meters
+        -length (float): length of the boat in meters
+        -shell (Polygon): a shapely Polygon object representing the boat's hull
+            -the shell is orientated according to the heading of the boat
 
-    The following information is obtained from AIS:
-    - ID (MMSI)
-    - position (lat/lon)
-    - speed (knots)
-    - heading (degrees measured clockwise from true north)
-
-
-
+    Notes
+    ----------
+        The following information about a boat is obtained from AIS:
+            - Maritime Mobile Service Identity (9 digit int)
+            - latitude (float)
+            - longitude (float)
+            - speed (knots)
+            - heading (degrees measured clockwise from true north)
+            - other information is also available but not used here
 
     """
 
-    def __init__(self, id, position, speed, heading) -> None:
+    def __init__(self, id, position, speed, true_bearing, width, length):
         """
+        Args:
 
-
-
-        Parameters
-        ----------
-        arg1 : type
-            Description of arg1.
-        arg2 : type
-            Description of arg2.
-
-        Attributes
-        ----------
-        attribute1 : type
-            Description of attribute1.
-        attribute2 : type
-            Description of attribute2.
+            id (int): MMSI number of the boat
+            -position (NamedTuple): x,y coordinates of the boat converted from lat/lon
+            -speed (float): speed of the boat in knots
+            -true_bearing (float, degrees): heading of the boat measured clockwise from true north
+            -width (float): width of the boat in meters
+            -length (float): length of the boat in meters
         """
         super().__init__(position)
         self.id = id
         self.speed = speed
-        self.heading = heading
+        self.true_bearing = true_bearing
+        self.width = width
+        self.length = length
+        self.shell = self.create_hull()
+
+    def create_hull(self, position, width, length, heading):
+        """
+        creates a Shapely Shell to represent the boat's hull, which is orientated and positioned
+        according to the boat's true bearing and position.
+
+        Args:
+
+            position (NamedTuple): x,y coordinates of the boat, converted from lat/lon
+            width (float): width of the boat in meters
+            length (float): length of the boat in meters
+            heading (float): heading of the boat in degrees measured clockwise from true north
+        """
+        # TODO implement this
