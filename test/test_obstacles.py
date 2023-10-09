d4dcadd60119907fbd2b055d68e4ff56441c507b
SPDonaghy <118148642+SPDonaghy@users.noreply.github.com>
2023-10-09 10:41:25 -0700

    fixed axes in the plotly chart

--- test/test_obstacles.py
+++ test/test_obstacles.py
@@ -1,9 +1,11 @@
 import numpy as np
 import plotly.graph_objects as go
-from shapely.geometry import Point
 
 from local_pathfinding.coord_systems import XY
 from local_pathfinding.obstacles import Boat
+
+# from shapely.geometry import Point
+
 
 position = XY(0, 0)
 
@@ -15,6 +17,8 @@ y = np.array(boat1.collision_cone.exterior.coords.xy[1])
 
 # Create a Plotly figure for boat1's collision cone with a hole representing the hull
 fig1 = go.Figure(go.Scatter(x=x, y=y, fill="toself"))
+
+fig1.update_layout(yaxis_range=[-200, 200], xaxis_range=[-200, 750])
 
 # Show the figures
 fig1.show()
