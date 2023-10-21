9ff6fa183e842f5e6fb315e21e0033f50df3eda1
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2023-10-21 13:41:15 -0700

    Mock tests for distance

--- test/test_objectives.py
+++ test/test_objectives.py
@@ -17,47 +17,61 @@ def test_distance_objective():
     distance_objective = objectives.DistanceObjective(PATH._simple_setup.getSpaceInformation())
     assert distance_objective is not None
 
-@pytest.mark.parametrize(
-    "s1,s2,is_valid",
-    [
-        (0.5, 0.5, True),
-        (0, 0.5, False),
-    ],
-)
-def test_get_path_length_objective(s1: tuple, s2: float, is_valid: bool):
-    state = ob.State(PATH._simple_setup.getStateSpace())
-    state().setXY(s1, s2)
+
+def test_get_path_length_objective(s1: tuple, s2: float):
+    space = ob.SE2StateSpace()
+
+    s1 = ob.State(space)
+    s1().setXY(0.5, 0.5)
+
+    s2 = ob.State(space)
+    s2().setXY(0.1, 0.2)
+
     dist_object = objectives.DistanceObjective(PATH._simple_setup.getSpaceInformation())
 
-    if is_valid:
-        assert dist_object.get_path_length_objective(state) == 0.5
-    else
-        assert not dist_object.get_path_length_objective(state) == 0.5
-
-
+    assert dist_object.get_ompl_path_length_objective() == 0
 
 
 @pytest.mark.parametrize(
-    "x,y,is_valid",
+    "s1,s2,expected",
     [
-        (0.5, 0.5, True),
-        (0.6, 0.6, False),
+        ((0, 0), (0, 0), 0),
+        ((0.5, 0.5), (0.1, 0.2), 0.5),
     ],
 )
-def test_get_euclidean_path_length_objective():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+def test_get_euclidean_path_length_objective(s1, s2, expected):
+    space = ob.SE2StateSpace()
+
+    s1 = ob.State(space)
+    s1().setXY(s1[0], s2[1])
+
+    s2 = ob.State(space)
+    s2().setXY(s2[0], s2[1])
+
+    dist_object = objectives.DistanceObjective(PATH._simple_setup.getSpaceInformation())
+
+    assert dist_object.get_ompl_path_length_objective(s1, s2) == expected
+
 
 @pytest.mark.parametrize(
-    "x,y,is_valid",
+    "x,y,expected",
     [
-        (0.5, 0.5, True),
-        (0.6, 0.6, False),
+        ((0, 0), (0, 0), 0),
+        ((13.205724, 29.828011), (13.205824, 29.828111), 0.01552),
     ],
 )
-def test_get_latlon_path_length_objective():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+def test_get_latlon_path_length_objective(s1, s2, expected):
+    space = ob.SE2StateSpace()
+
+    s1 = ob.State(space)
+    s1().setXY(s1[0], s1[1])
+
+    s2 = ob.State(space)
+    s2().setXY(s2[0], s2[1])
+
+    dist_object = objectives.DistanceObjective(PATH._simple_setup.getSpaceInformation())
+
+    assert dist_object.get_ompl_path_length_objective(s1, s2) == expected
 
 
 def test_minimum_turning_objective():
@@ -65,6 +79,7 @@ def test_minimum_turning_objective():
         PATH._simple_setup.getSpaceInformation(), PATH._simple_setup, PATH.state.headingDirection
     )
     assert minimum_turning_objective is not None
+
 
 @pytest.mark.parametrize(
     "x,y,is_valid",
@@ -77,6 +92,7 @@ def test_goalPathTurnCost():
     with pytest.raises(NotImplementedError):
         raise NotImplementedError
 
+
 @pytest.mark.parametrize(
     "x,y,is_valid",
     [
@@ -87,6 +103,7 @@ def test_goalPathTurnCost():
 def test_goalHeadingTurnCost():
     with pytest.raises(NotImplementedError):
         raise NotImplementedError
+
 
 @pytest.mark.parametrize(
     "x,y,is_valid",
