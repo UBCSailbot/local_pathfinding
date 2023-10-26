3e7082b971d05208930a6fd18fcf39273ba2cdfe
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2023-10-25 19:19:46 -0700

    Transfer of my work  from 25 heading objective branch

--- local_pathfinding/objectives.py
+++ local_pathfinding/objectives.py
@@ -1,6 +1,15 @@
 """Our custom OMPL optimization objectives."""
 
+import math
 from ompl import base as ob
+
+# Upwind downwind cost multipliers
+UPWIND_MULTIPLIER = 3000.0
+DOWNWIND_MULTIPLIER = 3000.0
+
+# Upwind downwind constants
+UPWIND_MAX_ANGLE_DEGREES = 40.0
+DOWNWIND_MAX_ANGLE_DEGREES = 20.0
 
 
 class Objective(ob.StateCostIntegralObjective):
@@ -20,61 +29,322 @@ class Objective(ob.StateCostIntegralObjective):
         self.space_information = space_information
 
     def motionCost(self, s1: ob.SE2StateSpace, s2: ob.SE2StateSpace) -> ob.Cost:
-        raise NotImplementedError
+        return ob.Cost(0.0)
 
 
 class DistanceObjective(Objective):
+    """Generates a distance objective function
+
+    Atributes:
+        space_information (class): The space information of the OMPL problem
+    """
+
     def __init__(self, space_information):
         super().__init__(space_information)
+        self.space_information = space_information
 
-    def motionCost(self, s1: ob.SE2StateSpace, s2: ob.SE2StateSpace) -> ob.Cost:
-        return ob.Cost()
+    def motionCost(self, s1, s2):
+        """Generates the distance between two points
 
-    def _get_path_length_objective(self):
-        raise NotImplementedError
+        Args:
+            s1 (SE2StateInternal): The starting point of the local start state
+            s2 (SE2StateInternal): The ending point of the local goal state
 
-    def _get_euclidean_path_length_objective(self, s1, s2):
-        raise NotImplementedError
+        Returns:
+            class/int: The distance between two points object or integer
+                       (currently it is returning a object)
+        """
 
-    def _get_latlon_path_length_objective(self, s1, s2):
-        raise NotImplementedError
+        # Generates the euclidean distance between two points
+        # euclideanPathObjective = self.get_euclidean_path_length_objective(s1, s2)
+
+        # Generates the latlon distance between two points
+        latlonPathObjective = self.get_latlon_path_length_objective(s1, s2)
+
+        return latlonPathObjective
+
+    def get_ompl_path_length_objective(self):
+        """Generates an OMPL Path Length Objective
+
+        Returns:
+            PathLengthOptimizationObjective: An OMPL path length objective object
+        """
+        path_objective = ob.PathLengthOptimizationObjective(self.space_information)
+
+        return path_objective
+
+    def get_euclidean_path_length_objective(self, s1, s2):
+        """Generates the euclidean distance between two points
+
+        Args:
+            s1 (SE2StateInternal): The starting point of the local start state
+            s2 (SE2StateInternal): The ending point of the local goal state
+
+        Returns:
+            float: The euclidean distance between the two points
+        """
+
+        return ((s2.getY() - s1.getY()) ** 2.0 + (s2.getX() - s1.getX()) ** 2.0) ** (0.5)
+
+    def get_latlon_path_length_objective(self, s1, s2):
+        """Generates the "great circle" distance between two points
+
+        I am assuming that we are using the lat and long coordinates in determining the distance
+        between two points.
+
+        Returns:
+            float: The great circle distance between two points
+        """
+
+        return (
+            math.acos(
+                (
+                    math.sin(math.radians(s1.getX())) * math.sin(math.radians(s2.getX()))
+                    + math.cos(math.radians(s1.getX())) * math.cos(math.radians(s2.getX()))
+                )
+                * math.cos(math.radians(s1.getY() - s2.getY()))
+            )
+            * 6378
+        )
 
 
 class MinimumTurningObjective(Objective):
-    def __init__(self, space_information, simple_setup, heading_degrees: float):
+    """Generates a minimum turning objective function
+
+    Attributes:
+        space_information (class): The space information of the OMPL problem
+        goal_x (float): The x coordinate of the goal state
+        goal_y (float): The y coordinate of the goal state
+        heading_degrees (float): The heading of the sailbot in degrees
+    """
+
+    def __init__(self, space_information, simple_setup, heading_degrees):
         super().__init__(space_information)
-        self.simple_setup = simple_setup
-        self.heading_degrees = heading_degrees
+        self.space_information = space_information
+        self.goal_x = simple_setup.getGoal().getState().getX()
+        self.goal_y = simple_setup.getGoal().getState().getY()
+        self.heading_degrees = math.radians(heading_degrees)
 
-    def motionCost(self, s1: ob.SE2StateSpace, s2: ob.SE2StateSpace) -> ob.Cost:
-        return ob.Cost()
+    def motionCost(self, s1, s2):
+        """Generates the turning cost between s1, s2, heading or the goal position
 
-    def _goal_path_turn_cost(self, s1, s2):
-        raise NotImplementedError
+        Args:
+            s1 (SE2StateInternal): The starting point of the local start state
+            s2 (SE2StateInternal): The ending point of the local goal state
 
-    def _goal_heading_turn_cost(self, s1):
-        raise NotImplementedError
+        Returns:
+            float: The minimum turning angle (degrees)
+        """
+        # Calculate the mininum turning cost between s1-goal and heading
+        # s1_goal__heading = self.goalHeadingTurnCost(s1)
 
-    def _heading_path_turn_cost(self, s1, s2):
-        raise NotImplementedError
+        # Calculate the minimum turning cost between s1-s2 and s1-goal
+        s1_s2__s1_goal = self.goalPathTurnCost(s1, s2)
+
+        # Calculate the minimum turning cost from sl-s2 and heading
+        # s1_s2__heading = self.headingPathTurnCost(s1, s2)
+
+        return s1_s2__s1_goal
+
+    def goalPathTurnCost(self, s1, s2):
+        """Determine the smallest turn angle between s1-s2 and s1-goal
+
+        Args:
+            s1 (SE2StateInternal): The starting point of the local start state
+            s2 (SE2StateInternal): The ending point of the local goal state
+
+        Returns:
+            float: the turning angle from s2 to s1 (degrees)
+        """
+        large_turn_threshold = math.pi / 2
+
+        # Calculate the angle of the s1-s2 line segment from North
+        path_direction = math.atan2(s2.getX() - s1.getX(), s2.getY() - s1.getY())
+
+        # Calculate the angle from s1-goal line segment from North
+        global_goal_direction = math.atan2(self.goal_x - s1.getX(), self.goal_y - s1.getY())
+
+        # Calculate the uncorrected turn size [0, 2pi]
+        turn_size_bias = math.fabs(global_goal_direction - path_direction)
+
+        # Correct the angle in between [0, pi]
+        if turn_size_bias > math.pi:
+            turn_size_unbias = turn_size_bias - 2 * math.pi
+        else:
+            turn_size_unbias = turn_size_bias
+
+        if turn_size_unbias > large_turn_threshold:
+            return math.degrees(math.fabs(turn_size_unbias))
+
+        return math.degrees(math.fabs(turn_size_unbias))
+
+    def goalHeadingTurnCost(self, s1):
+        """Determine the smallest turn angle between s1-s2 and heading
+
+        Args:
+            s1 (SE2StateInternal): The starting point of the local start state
+            s2 (SE2StateInternal): The ending point of the local goal state
+
+        Returns:
+            float: the turning angle from s2 to s1 (degrees)
+        """
+        large_turn_threshold = math.pi / 2
+
+        # Calculate the angle from s1-goal line segment from North
+        global_goal_direction = math.atan2(self.goal_x - s1.getX(), self.goal_y - s1.getY())
+
+        # Calculate the uncorrected turn size [0, 2pi]
+        turn_size_bias = math.fabs(global_goal_direction - self.heading_degrees)
+
+        # Correct the angle in between [0, pi]
+        if turn_size_bias > math.pi:
+            turn_size_unbias = turn_size_bias - 2 * math.pi
+        else:
+            turn_size_unbias = turn_size_bias
+
+        if turn_size_unbias > large_turn_threshold:
+            return math.degrees(math.fabs(turn_size_unbias))
+
+        return math.degrees(math.fabs(turn_size_unbias))
+
+    def headingPathTurnCost(self, s1, s2):
+        """Generates the turning cost between s1-s2 and heading of the sailbot
+
+        Args:
+            s1 (SE2StateInternal): The starting point of the local start state
+            s2 (SE2StateInternal): The ending point of the local goal state
+
+        Returns:
+            float: The minimum turning angle between s1-s2 and heading  (degrees)
+        """
+        large_turn_threshold = math.pi / 2
+
+        # Calculate the angle of the s1-s2 line segment from North
+        path_direction = math.atan2(s2.getX() - s1.getX(), s2.getY() - s1.getY())
+
+        # Calculate turn size
+        turn_size_bias = math.fabs(path_direction - self.heading_degrees)
+
+        # Correct the angle in between [0, pi]
+        if turn_size_bias > math.pi:
+            turn_size_unbias = turn_size_bias - 2 * math.pi
+        else:
+            turn_size_unbias = turn_size_bias
+
+        if turn_size_bias > large_turn_threshold:
+            return math.degrees(math.fabs(turn_size_unbias))
+
+        return math.degrees(math.fabs(turn_size_unbias))
 
 
 class WindObjective(Objective):
-    def __init__(self, space_information, wind_direction_degrees: float):
+    """Generates a wind objective function
+
+    Attributes:
+        space_information (class): The space information of the OMPL problem
+    """
+
+    def __init__(self, space_information, wind_direction_degrees):
         super().__init__(space_information)
+        self.space_information = space_information
         self.wind_direction_degrees = wind_direction_degrees
 
     # This objective function punishes the boat for going up/downwind
-    def motionCost(self, s1: ob.SE2StateSpace, s2: ob.SE2StateSpace) -> ob.Cost:
-        return ob.Cost()
+    def motionCost(self, s1, s2):
+        """Generates the cost associated with the upwind and downwind directions of the boat in
+           relation to the wind
 
-    @staticmethod
-    def _is_upwind(wind_direction_radians: float, boat_direction_radians: float) -> bool:
-        raise NotImplementedError
+        Args:
+            s1 (SE2StateInternal): The starting point of the local start state
+            s2 (SE2StateInternal): The ending point of the local goal state
 
-    @staticmethod
-    def _is_downwind(wind_direction_radians: float, boat_direction_radians: float) -> bool:
-        raise NotImplementedError
+        Returns:
+            float: The cost of going upwind or downwind
+        """
+        distance = ((s2.getY() - s1.getY()) ** 2 + (s2.getX() - s1.getX()) ** 2) ** 0.5
+        boat_direction_radians = math.atan2(s2.getX() - s1.getX(), s2.getY() - s1.getY())
+
+        if isUpwind(math.radians(self.wind_direction_degrees), boat_direction_radians):
+            return UPWIND_MULTIPLIER * distance
+        elif isDownwind(math.radians(self.wind_direction_degrees), boat_direction_radians):
+            return DOWNWIND_MULTIPLIER * distance
+        else:
+            return 0.0
+
+
+def isUpwind(wind_direction_radians, boat_direction_radians):
+    """Determines whether the boat is upwind or not and its associated cost
+
+    Args:
+        wind_direction_radians (float): The true wind direction (radians). [-pi, pi)
+        boat_direction_radians (float): The direction of the boat (radians). [-pi, pi)
+
+    Returns:
+        bool: The cost associated with the upwind direction
+    """
+    theta_min = math.degrees(wind_direction_radians - math.radians(UPWIND_MAX_ANGLE_DEGREES))
+    theta_max = math.degrees(wind_direction_radians + math.radians(UPWIND_MAX_ANGLE_DEGREES))
+
+    return is_angle_between(theta_min, math.degrees(boat_direction_radians), theta_max)
+
+
+def isDownwind(wind_direction_radians, boat_direction_radians):
+    """Generates the cost associated with the downwind direction
+
+    Args:
+        wind_direction_radians (float): The true wind direction (radians). [-pi, pi)
+        boat_direction_radians (float)): The direction of the boat (radians). [-pi, pi)
+
+    Returns:
+        bool: The cost associated with the downwind direction
+    """
+    downwind_wind_direction_radians = math.radians(bound_to_180(wind_direction_radians + math.pi))
+
+    theta_min = math.degrees(
+        downwind_wind_direction_radians - math.radians(DOWNWIND_MAX_ANGLE_DEGREES)
+    )
+    theta_max = math.degrees(
+        downwind_wind_direction_radians + math.radians(DOWNWIND_MAX_ANGLE_DEGREES)
+    )
+
+    return is_angle_between(theta_min, math.degrees(boat_direction_radians), theta_max)
+
+
+def bound_to_180(angle):
+    """Bounds the provided angle between [-180, 180) degrees.
+
+    Args:
+        angle (float): The input angle in degrees.
+
+    Returns:
+        float: The bounded angle in degrees.
+    """
+    return angle - 360 * ((angle + 180) // 360)
+
+
+def is_angle_between(first_angle, middle_angle, second_angle):
+    """Determines whether an angle is between two other angles.
+
+    Args:
+        first_angle (float): The first bounding angle in degrees.
+        middle_angle (float): The angle in question in degrees.
+        second_angle (float): The second bounding angle in degrees.
+
+    Returns:
+        bool: True when `middle_angle` is not in the reflex angle of
+              `first_angle` and `second_angle`, false otherwise.
+    """
+    if first_angle < second_angle:
+        if (
+            second_angle - 180 == first_angle
+        ):  # Assume all angles are between first and second when 180 degrees apart
+            return middle_angle != first_angle and middle_angle != second_angle
+        elif second_angle - 180 < first_angle:
+            return middle_angle > first_angle and middle_angle < second_angle
+        else:
+            return middle_angle < first_angle or middle_angle > second_angle
+    else:
+        return is_angle_between(second_angle, middle_angle, first_angle)
 
 
 def get_sailing_objective(
--- test/test_objectives.py
+++ test/test_objectives.py
@@ -1,8 +1,20 @@
 import pytest
 from rclpy.impl.rcutils_logger import RcutilsLogger
 
+from ompl import base as ob
 import local_pathfinding.objectives as objectives
+from local_pathfinding.objectives import (
+    bound_to_180,
+    is_angle_between,
+)
+import math  # for math.degrees()
 import local_pathfinding.ompl_path as ompl_path
+
+
+# Upwind downwind cost multipliers
+UPWIND_MULTIPLIER = 3000.0
+DOWNWIND_MULTIPLIER = 3000.0
+
 
 PATH = ompl_path.OMPLPath(
     parent_logger=RcutilsLogger(),
@@ -17,42 +29,239 @@ def test_distance_objective():
 
 
 def test_get_path_length_objective():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+    dist_object = objectives.DistanceObjective(PATH._simple_setup.getSpaceInformation())
+
+    assert dist_object.get_ompl_path_length_objective() is not None
 
 
-def test_get_euclidean_path_length_objective():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+@pytest.mark.parametrize(
+    "cs1,cs2,expected",
+    [
+        ((0, 0), (0, 0), 0),
+        ((0.5, 0.5), (0.1, 0.2), 0.5),
+    ],
+)
+def test_get_euclidean_path_length_objective(cs1, cs2, expected):
+    space = ob.SE2StateSpace()
+
+    s1 = ob.State(space)
+    s1().setXY(cs1[0], cs1[1])
+
+    s2 = ob.State(space)
+    s2().setXY(cs2[0], cs2[1])
+
+    dist_object = objectives.DistanceObjective(PATH._simple_setup.getSpaceInformation())
+
+    assert dist_object.get_euclidean_path_length_objective(s1(), s2()) == expected
 
 
-def test_get_latlon_path_length_objective():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+@pytest.mark.parametrize(
+    "cs1,cs2,expected",
+    [
+        ((0, 0), (0, 0), 0),
+        ((13.205724, 29.828011), (13.205824, 29.828111), 0.01552),
+    ],
+)
+def test_get_latlon_path_length_objective(cs1, cs2, expected):
+    space = ob.SE2StateSpace()
+
+    s1 = ob.State(space)
+    s1().setXY(cs1[0], cs1[1])
+
+    s2 = ob.State(space)
+    s2().setXY(cs2[0], cs2[1])
+
+    dist_object = objectives.DistanceObjective(PATH._simple_setup.getSpaceInformation())
+
+    assert round(dist_object.get_latlon_path_length_objective(s1(), s2()), 2) == round(expected, 2)
 
 
 def test_minimum_turning_objective():
     minimum_turning_objective = objectives.MinimumTurningObjective(
-        PATH._simple_setup.getSpaceInformation(), None, 0
+        PATH._simple_setup.getSpaceInformation(), PATH._simple_setup, PATH.state.headingDirection
     )
     assert minimum_turning_objective is not None
 
 
-def test_goalPathTurnCost():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+@pytest.mark.parametrize(
+    "cs1,cs2,sf,expected",
+    [
+        ((0, 0), (0, 0), (0, 0), 0),
+        ((-1, -1), (2, 1), (0.1, 0.2), 13.799),
+    ],
+)
+def test_goalPathTurnCost(cs1: tuple, cs2: tuple, sf: tuple, expected: float):
+    space = ob.SE2StateSpace()
+
+    goal = ob.State(space)
+
+    s1 = ob.State(space)
+    s2 = ob.State(space)
+
+    goal().setXY(sf[0], sf[1])
+    PATH._simple_setup.setGoalState(goal)
+
+    s1().setXY(cs1[0], cs1[1])
+    s2().setXY(cs2[0], cs2[1])
+
+    minimum_turning_objective = objectives.MinimumTurningObjective(
+        PATH._simple_setup.getSpaceInformation(), PATH._simple_setup, PATH.state.headingDirection
+    )
+    assert minimum_turning_objective.goalPathTurnCost(s1(), s2()) == pytest.approx(
+        expected, abs=1e-3
+    )
 
 
-def test_goalHeadingTurnCost():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+@pytest.mark.parametrize(
+    "cs1,sf,heading,expected",
+    [
+        ((0, 0), (0, 0), 0, 0),
+        ((-1, -1), (0.1, 0.2), 45, 2.490),
+    ],
+)
+def test_goalHeadingTurnCost(cs1: tuple, sf: tuple, heading: float, expected: float):
+    space = ob.SE2StateSpace()
+
+    goal = ob.State(space)
+
+    s1 = ob.State(space)
+
+    goal().setXY(sf[0], sf[1])
+    PATH._simple_setup.setGoalState(goal)
+
+    s1().setXY(cs1[0], cs1[1])
+
+    PATH.state.headingDirection = heading
+
+    minimum_turning_objective = objectives.MinimumTurningObjective(
+        PATH._simple_setup.getSpaceInformation(), PATH._simple_setup, PATH.state.headingDirection
+    )
+    assert minimum_turning_objective.goalHeadingTurnCost(s1()) == pytest.approx(expected, abs=1e-3)
 
 
-def test_headingPathTurnCost():
-    with pytest.raises(NotImplementedError):
-        raise NotImplementedError
+@pytest.mark.parametrize(
+    "cs1,cs2,heading,expected",
+    [
+        ((0, 0), (0, 0), 0.0, 0),
+        ((-1, -1), (2, 1), 45.0, 11.310),
+    ],
+)
+def test_headingPathTurnCost(cs1: tuple, cs2: tuple, heading: float, expected: float):
+    space = ob.SE2StateSpace()
+
+    s1 = ob.State(space)
+    s2 = ob.State(space)
+
+    s1().setXY(cs1[0], cs1[1])
+    s2().setXY(cs2[0], cs2[1])
+
+    PATH.state.headingDirection = heading
+
+    minimum_turning_objective = objectives.MinimumTurningObjective(
+        PATH._simple_setup.getSpaceInformation(), PATH._simple_setup, PATH.state.headingDirection
+    )
+    assert minimum_turning_objective.headingPathTurnCost(s1(), s2()) == pytest.approx(
+        expected, abs=1e-3
+    )
 
 
-def test_wind_objective():
-    wind_objective = objectives.WindObjective(PATH._simple_setup.getSpaceInformation(), 0)
-    assert wind_objective is not None
+@pytest.mark.parametrize(
+    "cs1,cs2,windDirection,expected",
+    [
+        ((0, 0), (0, 0), 0.0, 0 * UPWIND_MULTIPLIER),
+        ((-1, -1), (2, 1), 45.0, 3.605551275 * UPWIND_MULTIPLIER),
+    ],
+)
+def test_wind_objective(cs1: tuple, cs2: tuple, windDirection: float, expected: float):
+    space = ob.SE2StateSpace()
+
+    s1 = ob.State(space)
+    s2 = ob.State(space)
+
+    s1().setXY(cs1[0], cs1[1])
+    s2().setXY(cs2[0], cs2[1])
+
+    PATH.state.windDirection = windDirection
+
+    wind_objective = objectives.WindObjective(
+        PATH._simple_setup.getSpaceInformation(), PATH.state.windDirection
+    )
+
+    assert wind_objective.motionCost(s1(), s2()) == pytest.approx(expected, abs=1e-3)
+
+
+@pytest.mark.parametrize(
+    "windDirection,heading,expected",
+    [
+        (0, 0.0, True),
+        (0.0, 45.0, False),
+    ],
+)
+def test_isUpwind(windDirection: float, heading: float, expected: float):
+    PATH.state.headingDirection = heading
+    PATH.state.windDirection = windDirection
+
+    assert (
+        objectives.isUpwind(
+            math.radians(PATH.state.windDirection), math.radians(PATH.state.headingDirection)
+        )
+        == expected
+    )
+
+
+@pytest.mark.parametrize(
+    "windDirection,heading,expected",
+    [
+        (0.0, 0.0, True),
+        (25.0, 45.0, False),
+    ],
+)
+def test_isDownwind(windDirection: float, heading: float, expected: float):
+    PATH.state.headingDirection = heading
+    PATH.state.windDirection = windDirection
+
+    assert (
+        objectives.isDownwind(
+            math.radians(PATH.state.windDirection), math.radians(PATH.state.headingDirection)
+        )
+        == expected
+    )
+
+
+""" Tests for bound_to_180() """
+
+
+def test_bound_basic1():
+    assert bound_to_180(0) == 0
+    assert bound_to_180(-180) == -180
+    assert bound_to_180(180) == -180
+    assert bound_to_180(540) == -180
+    assert bound_to_180(720) == 0
+    assert bound_to_180(60) == 60
+    assert bound_to_180(-45) == -45
+    assert bound_to_180(120) == 120
+    assert bound_to_180(-125) == -125
+    assert bound_to_180(179) == 179
+    assert bound_to_180(-179) == -179
+    assert bound_to_180(-3779) == -179
+    assert bound_to_180(3779) == 179
+    assert bound_to_180(360) == 0
+
+
+""" Tests for is_angle_between() """
+
+
+def test_between_basic1():
+    """Checks different situations such as boundary conditions.
+    For example, what happens when par1 == par2 == par3?
+    In addition, what happens if we change the order of the parameters
+    """
+    assert is_angle_between(0, 1, 2) == 1
+    assert is_angle_between(0, 20, 360) == 0
+    assert is_angle_between(-20, 10, 40) == 1
+    assert is_angle_between(0, 30, 60) == 1
+
+    # Checks negative angle conditions such as can it identify a negative angle between a positive
+    # and negative angle.
+    assert is_angle_between(-170, -130, -90) == 1
+    assert is_angle_between(-170, -130, 100) == 0
