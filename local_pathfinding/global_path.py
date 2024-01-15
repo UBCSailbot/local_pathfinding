629085bd76edbad77d742a93ca70aa3c4da1d05e
SPDonaghy <118148642+SPDonaghy@users.noreply.github.com>
2024-01-15 15:10:30 -0800

    fixed spamming POST request bug, works now just needs a lookover and some test cases

--- local_pathfinding/global_path.py
+++ local_pathfinding/global_path.py
@@ -19,33 +19,35 @@ from local_pathfinding.node_mock_global_path import (
     path_to_dict,
 )
 
+# TODO Everyrhing works as expected. just need to write some tests and write up docs
+
 GPS_URL = "http://localhost:3005/api/gps"
 PATH_URL = "http://localhost:8081/global-path"
+GLOBAL_PATHS_FILE_PATH = "/workspaces/sailbot_workspace/src/local_pathfinding/global_paths"
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", help="The path to the global path csv file.")
     parser.add_argument("--interval", help="Desired path interval length.", type=float)
-    parser.add_argument(
-        "--write",
-        help="Whether to overwrite the path with the formatted path",
-        action="store_true",
-    )
+
     args = parser.parse_args()
     path_mod_tmstmp = None
+    file_path = args.file_path
+
     try:
-        print(path_to_dict(get_path(args.file_path)))
+        print(path_to_dict(get_path(file_path)))
     except FileNotFoundError:
         print("File not found. Please enter a valid file path.")
         exit(1)
 
-    # Continuosly check if the global path should be updated
+    # Check if the global path should be updated
     while True:
-        # check when global path was changed last
-        timestamp = time.ctime(os.path.getmtime(args.file_path))
+        timestamp = time.ctime(os.path.getmtime(file_path))
 
+        # We should try to retrieve the position on every loop
         position = None
+
         try:
             position = json.loads(urlopen("http://localhost:3005/api/gps").read())
         except HTTPError as http_error:
@@ -60,7 +62,7 @@ def main():
 
         lat_lon = position["lat_lon"]
         pos = HelperLatLon(latitude=lat_lon["latitude"], longitude=lat_lon["longitude"])
-        path = get_path(args.file_path)
+        path = get_path(file_path)
 
         position_delta = meters_to_km(
             GEODESIC.inv(
@@ -78,17 +80,44 @@ def main():
             continue
 
         if args.interval is not None:
+            # format path will interpolate new path and save it to a new csv file
             path = format_path(
                 path=path,
                 pos=pos,
                 interval_spacing=args.interval,
-                file_path=args.file_path,
-                write=args.write,
+                file_path=file_path,
             )
 
         post_path(path)
 
         path_mod_tmstmp = timestamp
+
+        # An alternate option is to have the functions generate_path and interpolate_path return
+        # a tuple containing the new path and the file path of the new csv file
+        # but this would be a breaking change to multiple files, so this is a quick workaround
+        file_path = get_most_recent_file(GLOBAL_PATHS_FILE_PATH)
+
+        # we don't need to update multiple times per second
+        time.sleep(1)
+
+
+def get_most_recent_file(directory_path: str) -> str:
+    all_files = os.listdir(directory_path)
+
+    # Filter out directories and get the full file paths
+    files = [
+        os.path.join(directory_path, file)
+        for file in all_files
+        if os.path.isfile(os.path.join(directory_path, file))
+    ]
+
+    # Sort the files based on their last modification time
+    files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+
+    if files:
+        return files[0]
+    else:
+        return ""
 
 
 def get_path(file_path: str) -> Path:
@@ -129,9 +158,7 @@ def post_path(path: Path):
         print(f"An unexpected error occurred: {e}")
 
 
-def format_path(
-    path: Path, pos: HelperLatLon, interval_spacing: float, file_path: str, write: bool
-) -> Path:
+def format_path(path: Path, pos: HelperLatLon, interval_spacing: float, file_path: str) -> Path:
     """Interpolates path to ensure the interval lengths are less than or equal to the specified
     interval spacing.
 
@@ -152,7 +179,7 @@ def format_path(
             dest=path.waypoints[0],
             interval_spacing=interval_spacing,
             pos=pos,
-            write=write,
+            write=True,
             file_path=file_path,
         )
     # Check if any waypoints are too far apart
@@ -162,7 +189,7 @@ def format_path(
             interval_spacing=interval_spacing,
             pos=pos,
             path_spacing=path_spacing,
-            write=write,
+            write=True,
             file_path=file_path,
         )
 
